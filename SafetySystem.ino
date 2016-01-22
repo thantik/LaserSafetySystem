a05c5ba43e3bfa3d801865761c7a4129844be532
Anthony Bolgar <anthonybolgar@live.com>
2016-01-22 01:37:21 -0500

    Update SafetySystem.ino

--- SafetySystem.ino
+++ SafetySystem.ino
@@ -7,11 +7,11 @@ LCD is a common 20x4 lcd panel driven by an I2C board.
 
 Pin Assignments for Arduino Nano board:
 
-D2 - Coolant flow monitor (flow meter output to D2)
+D2 - Coolant flow monitor (flow meter output{orange wire} to D2, red wire to +5V, black wire to ground))
 D3 - Door Switch (Hook up switch between ground and D3)
      (You can add additional microswitches in series to protect the laser tube door and controls door)
 D4 - Warning buzzer or status LED (Buzzer+ to D4, buzzer- to ground)
-D5 - Relay trigger to diasable laser fire (+5V to relay VCC,D5 connects to Relay IN,
+D5 - Relay trigger to diasable laser fire (+5V to relay VCC, D5 connects to Relay IN,
      one side of laser enable to relay common, othe side of laser enable to relay NO)
 A0 - Coolant temperature monitor (Sensor - TMP36) (Pin1 to A0, Pin2 to +5V, Pin3 to ground)
 
